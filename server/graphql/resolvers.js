5205365f0743d3ccc662a7ffec81032e70784b20
Jhonny Rodriguez Martinez <71800608+Jyro77@users.noreply.github.com>
2023-02-03 21:58:16 -0400

    Delete, Update and Mutations
    Complete the Crud system
    fix delete process for one to many
    add mutations for better use of the dates
    use type definitions to mutate params

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -5,7 +5,9 @@ export const resolvers = {
     Query: {
         hello: () => "Hello World!",
         projects: async () => await Project.find(),
+        project: async (_, { _id }) => await Project.findById(_id),
         tasks: async () => await Task.find(),
+        task: async (_, { _id }) => await Task.findById(_id),
     },
     Mutation: {
         createProject: async (_, { name, description }) => {
@@ -15,6 +17,21 @@ export const resolvers = {
             });
             const savedProject = await project.save();
             return savedProject;
+        },
+        deleteProject: async (_, { _id }) => {
+            const deletedProject = await Project.findByIdAndDelete(_id);
+            if (!deletedProject) throw new Error("Project not found");
+            await Task.deleteMany({ projectId: deletedProject._id });
+            return deletedProject;
+        },
+        updateProject: async (_, args) => {
+            const updatedProject = await Project.findByIdAndUpdate(
+                args._id,
+                args,
+                { new: true },
+            );
+            if (!updatedProject) throw new Error("Project not found");
+            return updatedProject;
         },
         createTask: async (_, { title, projectId }) => {
             const projectFound = await Project.findById(projectId);
@@ -27,5 +44,23 @@ export const resolvers = {
             const savedTask = await task.save();
             return savedTask;
         },
+        deleteTask: async (_, { _id }) => {
+            const deletedTask = await Task.findByIdAndDelete(_id);
+            if (!deletedTask) throw new Error("Task not found");
+            return deletedTask;
+        },
+        updateTask: async (_, args) => {
+            const updatedTask = await Task.findByIdAndUpdate(args._id, args, {
+                new: true,
+            });
+            if (!updatedTask) throw new Error("Task not found");
+            return updatedTask;
+        },
+    },
+    Project: {
+        tasks: async parent => await Task.find({ projectId: parent._id }),
+    },
+    Task: {
+        project: async parent => await Project.findById(parent.projectId),
     },
 };
