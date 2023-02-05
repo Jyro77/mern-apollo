605d8339815245bb6fbbce3820246b5bbc4067c4
Jhonny Rodriguez Martinez <71800608+Jyro77@users.noreply.github.com>
2023-02-05 00:00:31 -0400

    Form Component with TS

--- client/src/components/ProjectForm.tsx
+++ client/src/components/ProjectForm.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState, ChangeEvent, SyntheticEvent } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_PROJECT, GET_PROJECTS } from "../graphql/projects";
 
 function ProjectForm() {
-    return <div>ProjectForm</div>;
+    const [project, setProject] = useState<{
+        name: string;
+        description: string;
+    }>({
+        name: "",
+        description: "",
+    });
+
+    const [createProject, { loading, error }] = useMutation(CREATE_PROJECT, {
+        variables: {
+            name: project.name,
+            description: project.description,
+        },
+        refetchQueries: [
+            {
+                query: GET_PROJECTS,
+            },
+            "GetProjects",
+        ],
+    });
+
+    const handlerChange = (
+        e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
+    ) => {
+        setProject({
+            ...project,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handlerSubmit = (e: SyntheticEvent) => {
+        e.preventDefault();
+        createProject();
+        setProject({
+            name: "",
+            description: "",
+        });
+    };
+    return (
+        <form onSubmit={handlerSubmit}>
+            {error && <p>{error.message}</p>}
+            <input
+                type='text'
+                name='name'
+                placeholder='Title of project'
+                onChange={handlerChange}
+                value={project.name}
+            />
+            <textarea
+                name='description'
+                rows={3}
+                placeholder='Description...'
+                onChange={handlerChange}
+                value={project.description}></textarea>
+            <button disabled={!project.name || !project.description || loading}>
+                Submit
+            </button>
+        </form>
+    );
 }
 
 export default ProjectForm;
